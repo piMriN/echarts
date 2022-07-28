a892f4b342500a9fe4ccb3acc9a00221c3662f53
MaNa0923 <1491343463@qq.com>
2022-07-28 15:00:47 +0800

    '代码自动修复‘

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
-axios.defaults.baseURL='http://175.27.228.178:3000/api/'
-Vue.prototype.$http=axios
+axios.defaults.baseURL = 'http://175.27.228.178:3000/api/'
+Vue.prototype.$http = axios
 
 Vue.prototype.$echarts = window.echarts
 Vue.config.productionTip = false
@@ -12,5 +12,5 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer:{
-    port:8888,
-    open:false
+  devServer: {
+    port: 8888,
+    open: false
   }
 })
